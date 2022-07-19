07e8d93e9e60c8cc0f983c52ed42b108a7af5633
Alesouza7 <alexcostasouza86@yahoo.com>
2022-07-18 21:55:26 -0300

    alexsouzaexercicio

---
+++ script.js
@@ -0,0 +1,44 @@
+// a aula de hoje sera muito legal
+/*dsfffsds
+fefesf
+//exercicios
+let nome = prompt("Qual é o seu nome?")
+let idade = parseInt(prompt("Qual a sua idade?"))
+
+//nome = "Juliana"
+//idade = 38
+
+console.log("Oi meu nome é",nome,"eu tenho",idade,"anos.")
+
+let nomePet = false
+const nomedoPet = undefined
+
+console.log(nomePet)
+
+console.log (typeof nome)
+console.log (typeof idade)
+console.log (typeof nomePet)
+console.log (typeof nomedoPet)
+
+//exercicio de fixação 1
+
+let nome = "Alex"//string
+const idade = 38//number
+let falso = false//boolean
+
+console.log (typeof nome)
+console.log (typeof idade)
+console.log (typeof falso)*/
+
+let a = 30
+let b = 40
+let c = null
+
+c = a
+a = b
+b = c
+
+console.log("A",a)
+console.log("B",b)
+
+
